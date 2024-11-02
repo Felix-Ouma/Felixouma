53912c0fff74df87c52a5357280dc2e2aeb0fe88
Felix-ouma <felixouma859@gmail.com>
2024-11-02 21:10:58 +0300

    Adding Resume Section

--- src/Pages/Resume/CvButton.jsx
+++ src/Pages/Resume/CvButton.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 import styles from './CvButton.module.css';
 
 const CvButton = () => {
+  const handleDownload = () => {
+    window.open('https://drive.google.com/file/d/1_tnbXx7l9dTIQNVS_i4w4vB1WXsVUY57/view?usp=sharing', '_blank');
+  };
+
   return (
     <div className={styles.buttonContainer}>
-      <button className={styles.centerButton}>Download CV</button>
+      <button className={styles.centerButton} onClick={handleDownload}>
+        Download CV
+      </button>
     </div>
   );
 };
