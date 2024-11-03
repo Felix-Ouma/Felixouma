636a51388fe1f37a956734f90a39fc04ab7143ff
Felix-ouma <felixouma859@gmail.com>
2024-11-03 20:45:39 +0300

    Adding Resume Section

--- src/Pages/Contact/Conect.jsx
+++ src/Pages/Contact/Conect.jsx
@@ -5,9 +5,10 @@ function Conect () {
   return (
   <>
     <div className={styles.bitWork}>
-      <h2 className={styles.tittles}>Let's work together</h2>
+      <h2 className={styles.tittles}>Let's work Together</h2>
       <p className={styles.description}>
-      I'm always excited to work on the next big product! I'm open to full-time offers and long-term contracts., Please feel free to reach out </p>   </div>
+      I'm always excited to work on the next big product! I'm open to full-time offers and long-term contracts., Please feel free to reach out </p>  
+    </div>
   </>
   );
 }
