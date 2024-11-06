fb14e3c104e00dadf1f9b3deebb4d7e49eb23839
Felix-ouma <felixouma859@gmail.com>
2024-11-06 19:30:28 +0300

    Updating social Icons

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaLinkedin, FaDribbble, FaGithub, FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
+import { FaLinkedin, FaDribbble, FaGithub, FaFacebook} from "react-icons/fa";
 import styles from "./Footer.module.css";
 import WhatsAppButton from "./WhatsAppButton";
 
@@ -8,24 +8,19 @@ const Footer = () => {
     <footer className={styles.footer}>
       <WhatsAppButton />
       <div className={styles.socialLinks}>
-        <a href="https://www.linkedin.com/in/felixouma" target="_blank" rel="noopener noreferrer" className={styles.icon}>
-          <FaLinkedin />
-        </a>
-        <a href="https://dribbble.com/felolearn01" target="_blank" rel="noopener noreferrer" className={styles.icon}>
-          <FaDribbble />
-        </a>
-        <a href="https://www.facebook.com/profile.php?id=100091803387677" target="_blank" rel="noopener noreferrer" className={styles.icon}>
-          <FaFacebook />
-        </a>
-        <a href="https://github.com/Felix-Ouma" target="_blank" rel="noopener noreferrer" className={styles.icon}>
-          <FaGithub />
-        </a>
-        <a href="https://www.instagram.com/felixouma" target="_blank" rel="noopener noreferrer" className={styles.icon}>
-          <FaInstagram />
-        </a>
-        <a href="https://twitter.com/felixouma" target="_blank" rel="noopener noreferrer" className={styles.icon}>
-          <FaTwitter />
-        </a>
+      <a href="https://www.linkedin.com/in/felixouma" target="_blank" rel="noopener noreferrer" className={styles.icon}>
+            <FaLinkedin />
+          </a>
+          <a href="https://dribbble.com/felolearn01" target="_blank" rel="noopener noreferrer" className={styles.icon}>
+            <FaDribbble />
+          </a>
+          
+          <a href="https://github.com/Felix-Ouma" target="_blank" rel="noopener noreferrer" className={styles.icon}>
+            <FaGithub />
+          </a>
+          <a href="https://www.facebook.com/profile.php?id=100091803387677" target="_blank" rel="noopener noreferrer" className={styles.icon}>
+            <FaFacebook />
+          </a>
       </div>
       <p className={styles.footerText}>© {new Date().getFullYear()} Felix Ouma. All rights reserved. Other intellectual properties belong to their respective owners. Some case studies are modified or hidden due to non-disclosure agreements..</p>
     </footer>
